4cc7a6e4a156afaffb2efcd8a6310897fcc685a7
Kashyap <ckkashyap@gmail.com>
2010-11-11 23:12:05 +0530

    working version

--- RFB/CommandLoop.hs
+++ RFB/CommandLoop.hs
@@ -19,6 +19,7 @@ commandLoop handle = do
 	case command of
 		0 -> processSetPixelFormat commandData
 		2 -> processSetEncodings handle commandData 
+		3 -> processFrameBufferUpdateRequest handle commandData
 		_ -> putStrLn "Some other command"
 	
 	commandLoop handle
@@ -44,3 +45,21 @@ processSetEncodings handle [count] = do
 	putStrLn (show count)
 	x <- BS.hGet handle (4*count)
 	return ()
+
+
+blueScreen :: BS.ByteString
+blueScreen = runPut $ do
+	putWord8 0
+	putWord8 0
+	putWord16be 1
+	putWord16be 0
+	putWord16be 0
+	putWord16be 100
+	putWord16be 100
+	putWord32be 0
+	
+
+processFrameBufferUpdateRequest handle [x,y,width,height] = do
+	putStrLn ("FrameBufferUpdateRequest x=" ++ (show x) ++ ", y=" ++ (show y) ++ " width =" ++ (show width) ++ ", height=" ++ (show height))
+	BS.hPutStr handle blueScreen
+
